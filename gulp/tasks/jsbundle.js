b6c7b5a7a008cf5e9c68bee21b8af9b85efedf7a
Victor Montero <victor.nan0@gmail.com>
2016-03-03 11:37:38 -0600

    Remove unused  require task-listing

--- gulp/tasks/jsbundle.js
+++ gulp/tasks/jsbundle.js
@@ -9,8 +9,6 @@ const browserify  = require('browserify');
 const source      = require('vinyl-source-stream');
 const buffer      = require('gulp-buffer');
 const exorcist    = require('exorcist');
-const taskListing = require('gulp-task-listing');
-
 
 gulp.task('jsbundle', () => {
   return browserify({
