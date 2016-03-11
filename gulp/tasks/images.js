1a0c37726fcadf4660b9724bdb631b1311925387
Jose Pablo Granados <jpgcodecr@gmail.com>
2016-03-11 16:29:04 -0600

    Removing cache plugin from image task

--- gulp/tasks/images.js
+++ gulp/tasks/images.js
@@ -2,14 +2,13 @@
 
 const gulp     = require('gulp');
 const config   = require('../config');
-const cache    = require('gulp-cache');
 const imagemin = require('gulp-imagemin');
 
 gulp.task('images', ['clean:dist'],  () => {
     return gulp.src(config.images.all)
-        .pipe(cache(imagemin({
+        .pipe(imagemin({
             progressive: true,
             interlaced: true
-        })))
+        }))
         .pipe(gulp.dest(config.images.distDest));
 });
