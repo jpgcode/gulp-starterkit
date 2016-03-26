c4686571a8e1b1572013506713bbb1ba935adbb4
Jose Pablo Granados <jpgcodecr@gmail.com>
2016-03-26 12:25:02 -0600

    Fix: Travis CI notify environment

--- gulp/tasks/notify.js
+++ gulp/tasks/notify.js
@@ -2,9 +2,10 @@
 
 import gulp   from 'gulp';
 import config from '../config';
+import gutil  from 'gulp-util';
 import notify from 'gulp-notify';
 
 gulp.task('notify:server', () => {
   return gulp.src('./gulpfile.babel.js')
-      .pipe(notify('Server ready!'));
+      .pipe(gutil.env.type !== 'ci' ? notify('Server ready!') : gutil.noop())
 });
